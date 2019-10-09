c0fe16565f16f117dc9181dfef80c50612144b7d
Esteban MESTRIE <20190198@B15-16.local>
2019-10-09 09:39:32 +0200

    exo 17

---
+++ ex17.js
@@ -0,0 +1,5 @@
+module.exports = function indexOf(str = '', search, fromIndexotp = 0) 
+ {
+    let finish = str.indexOf(search);
+    return finish
+ }
--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 
-const lowerCase = require('./ex01')
-const upperCase = require('./ex02')
-const length = require('./ex03')
-const capitalize = require('./ex04')
-const decapitalize = require('./exo05')
-const kebabCase = require('./ex06')
-const snakecase = require('./ex07')
-const swapCase = require('./ex08')
-const charAt = require('./ex09')
-const first = require('./ex10')
-const last = require('./ex11')
-const substr = require('./ex12')
-const slice = require('./ex13')
-const count = require('./ex14')
-const countSubstrings = require('./ex15')
-const countWords = require('./ex16')
-
+const lowerCase         = require('./ex01')
+const upperCase         = require('./ex02')
+const length            = require('./ex03')
+const capitalize        = require('./ex04')
+const decapitalize      = require('./exo05')
+const kebabCase         = require('./ex06')
+const snakecase         = require('./ex07')
+const swapCase          = require('./ex08')
+const charAt            = require('./ex09')
+const first             = require('./ex10')
+const last              = require('./ex11')
+const substr            = require('./ex12')
+const slice             = require('./ex13')
+const count             = require('./ex14')
+const countSubstrings   = require('./ex15')
+const countWords        = require('./ex16')
+const indexOf           = require('./ex17')
 
 
 
@@ -123,6 +123,13 @@ console.log('Count Words :')
 console.log(countWords('Tony Tony Ch0pper'))
 console.log(countWords('TonyTonyCh0pper'))
 console.log(countWords('Tony Tony - Ch0pper'))
+
+console.log('____')
+
+console.log('Index of :')
+console.log(indexOf('Ch0pper', '0'))
+console.log(indexOf('Ch0pper', 'o'))
+
 
 
 
