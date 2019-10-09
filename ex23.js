1368efb175c77d5a0a58e251b74d5d242b1fb884
Esteban MESTRIE <20190198@B15-16.local>
2019-10-09 11:18:53 +0200

    exo 23

--- ex23.js
+++ ex23.js
@@ -1,7 +1,5 @@
 module.exports = function trim(str = '', whitespaceopt = '') 
  {
-     
     let replace = new RegExp (whitespaceopt, "g")
     return finish = str.replace(replace, '').trim()
-
  }
